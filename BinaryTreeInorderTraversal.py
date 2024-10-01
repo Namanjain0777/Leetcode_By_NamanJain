1c5509036b46045cda8e07659a0e80e6cd9b3d99
Naman Jain <Namanjainp01@gmail.com>
2024-10-01 18:24:17 +0530

    Create BinaryTreeInorderTraversal.py

---
+++ BinaryTreeInorderTraversal.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        
+        inorder(root)
+        return res
