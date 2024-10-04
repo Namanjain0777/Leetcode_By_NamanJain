42f7fd020ad53b12fdd7ef60c7aedca769755736
Naman Jain <Namanjainp01@gmail.com>
2024-10-04 10:28:51 +0530

    Create SwapNodesInPairs.py

---
+++ SwapNodesInPairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev, cur = dummy, head
+
+        while cur and cur.next:
+            npn = cur.next.next
+            second = cur.next
+
+            second.next = cur
+            cur.next = npn
+            prev.next = second
+
+            prev = cur
+            cur = npn
+        
+        return dummy.next
