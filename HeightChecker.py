17475563570f6f71693a23d1cf23a8c0ec9cb520
Naman Jain <Namanjainp01@gmail.com>
2024-10-08 17:46:28 +0530

    Create HeightChecker.py

---
+++ HeightChecker.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def heightChecker(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        freq=[0]*101
+        for x in heights:
+            freq[x]+=1
+        cnt=0
+        for x in range(1, 101):
+            f=freq[x]
+            for i in range(cnt, cnt+f):
+                heights[i]-=x
+            cnt+=f
+        return len(heights)-heights.count(0)
+        
+        
