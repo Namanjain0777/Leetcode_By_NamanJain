5eda978f424da06ee2393e0d33225ecf8c6f39a2
Naman Jain <Namanjainp01@gmail.com>
2024-10-26 13:30:52 +0530

    Create Check if Bitwise OR Has Trailing Zeros.py

---
+++ Check if Bitwise OR Has Trailing Zeros.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasTrailingZeros(self, nums):
+        count = 0
+
+        for num in nums:
+            if num % 2 == 0:
+                count += 1
+            
+            if count == 2:
+                return True
+
+        return False
