e29f2376463e66f2cc5ce25e1d9808b6be08cd35
Naman Jain <Namanjainp01@gmail.com>
2024-11-06 14:30:08 +0530

    Create Longest Harmonious Subsequence.py

---
+++ Longest Harmonious Subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLHS(self, nums) :
+        freq = Counter(nums)
+        max_length = 0
+        
+        for key in freq:
+            if key + 1 in freq:
+                max_length = max(max_length, freq[key] + freq[key+1])
+                
+        return max_length
